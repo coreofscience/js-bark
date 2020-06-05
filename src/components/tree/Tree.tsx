cecf267fcfa747f15519c48d0b4cab65b35bb826
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-06-05 00:03:50 -0500

    Add some new types and props

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -2,15 +2,55 @@ import React, { FC } from "react";
 import range from "lodash.range";
 import "./Tree.css";
 
-const Tree: FC = () => {
+interface Info {
+  title: string;
+  description: string;
+}
+
+interface SectionInfo {
+  root: Info;
+  trunk: Info;
+  leaf: Info;
+}
+
+const INFO: SectionInfo = {
+  root: {
+    title: "Root",
+    description:
+      "Here you should find seminal articles from the original articles of your topic of interest.",
+  },
+  trunk: {
+    title: "Trunk",
+    description:
+      "Here you should find articles where your topic of interest got a structure, these should be the first authors to discover the applicability of your topic of interest",
+  },
+  leaf: {
+    title: "Leaves",
+    description:
+      "Here you should find recent articles and reviews that should condense very well your topics.",
+  },
+};
+
+interface Article {
+  label: string;
+  title?: string;
+  authors: string[];
+  year?: number;
+}
+
+type Props = {
+  [k in keyof SectionInfo]: Article[];
+};
+
+const Tree: FC<Props> = (props) => {
   return (
     <div className="Tree">
-      {["root", "trunk", "leave"].map((section) => (
+      {(Object.keys(INFO) as Array<keyof SectionInfo>).map((section) => (
         <div className={`Tree-segment ${section}`}>
           <div className="Tree-segment-info">
             <div className="Info">
-              <h3>Info</h3>
-              <p>Info body</p>
+              <h3>{INFO[section]["title"]}</h3>
+              <p>{INFO[section]["description"]}</p>
             </div>
           </div>
           <div className="Tree-segment-articles">
